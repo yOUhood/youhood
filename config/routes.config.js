b595067c38ead36efba5f0177fe9d3083acdff1f
Manuela Ghiulai <gmanuelaromina@gmail.com>
2021-06-25 13:41:54 +0200

    feat: added doRegister and action/method to register form

--- config/routes.config.js
+++ config/routes.config.js
@@ -3,5 +3,6 @@ const auth = require("../controllers/auth.controller");
 const router = express.Router();
 
 router.get("/register", auth.register);
+router.post("/register", auth.doRegister);
 
 module.exports = router;
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,3 +1,9 @@
 module.exports.register = (req, res, next) => {
     res.render('auth/register')
 }
+
+module.exports.doRegister = (req, res, next) => {
+    res.render('auth/register', {
+        user: req.body,
+    })
+}
